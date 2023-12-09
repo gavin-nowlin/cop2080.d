fc0ae61144e25418822c30a9be6ed7490937229b
Taiji <Hashimototaiji@gmail.comm>
2023-12-09 13:30:39 -0500

    .

--- controller.py
+++ controller.py
@@ -5,12 +5,13 @@ from model import *
 from view import *
 
 def main():
-    file_types = ["*.wav", "*.mp3"]
-    file_path = filedialog.askopenfilename(title="Select Audio File", filetypes=[("Audio Files", file_types)])
-    #checks if user selects a file
-    if file_path:
-        calculate_rt60(file_path)
-    # file_gui()
+    # file_types = ["*.wav", "*.mp3"]
+    # file_path = filedialog.askopenfilename(title="Select Audio File", filetypes=[("Audio Files", file_types)])
+    # #checks if user selects a file
+    # if file_path:
+    #     calculate_rt60(file_path)
+        
+    file_gui()
 
 if __name__ == "__main__":
     main()
